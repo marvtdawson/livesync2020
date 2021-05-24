a32987bf90b09ae629993dc362e75349d83f19de
Marvin Dawson <marvintdawson@gmail.com>
2021-05-24 07:27:11 -0500

    updating app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import {Capacitor, Plugins} from '@capacitor/core';
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Platform, ToastController} from "@ionic/angular";
+import { Plugins } from '@capacitor/core';
+const { Device } = Plugins;
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+    pageTitle = 'Home';
+
+    constructor(public platform: Platform,
+                public toastController: ToastController) {  }
+
+    ngOnInit() {
+        const info = Device.getInfo()
+            .then(data => {
+                console.log('Device is: ' + info );
+            })
+            .catch();
+
+        const platformType = this.platform.is("android");
+        console.log(platformType);
+        if (platformType) {
+            this.presentToast().then().catch();
+        }
+    }
+
+    async presentToast() {
+        const currPlatform = this.platform.platforms();
+        console.log(currPlatform);
+        const toast = await this.toastController.create({
+            message: currPlatform.toString(),
+            duration: 2500
+        });
+        toast.present().then().catch();
+    }
 
 }
