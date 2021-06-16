091b9690a63b098f113c46a5ba5ba90fd59225a8
Marvin Dawson <marvintdawson@gmail.com>
2021-06-16 09:28:40 -0500

    added footer to app

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {Observable} from "rxjs";
 import {SiteDataService} from "../../../services/site-data/site-data.service";
 
@@ -10,14 +10,28 @@ import {SiteDataService} from "../../../services/site-data/site-data.service";
 export class HeaderComponent implements OnInit {
 
   @Input()pageTitle: string;
-
+  dropdown = true;
+  accountDropDown = true;
   appName = this.siteData.siteName;
   appLogo = this.siteData.siteLogo;
   isUserLoggedOut$: Observable<boolean>;
   isUserLoggedIn$: Observable<boolean>;
   displayName$: any;
+
+  @ViewChild('eventsBtn', {read: ElementRef}) eventBtn: ElementRef;
+  @ViewChild('accountBtn', {read: ElementRef}) accountBtn: ElementRef;
+
   constructor(private siteData: SiteDataService) { }
 
   ngOnInit() {}
+
+  hideDropdown(event) {
+    const xTouch = (event.clientX).toFixed(2);
+    const yTouch = (event.clientY).toFixed(2);
+
+    const rect = this.eventBtn.nativeElement.getBoundingClientRect();
+  }
+
+  hideAccountDropdown(event) {}
 
 }
--- src/app/layout/footer/footer.page.ts
+++ src/app/layout/footer/footer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SiteDataService} from "../../../services/site-data/site-data.service";
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterPage implements OnInit {
 
-  constructor() { }
+  siteName = this.siteData.siteName;
+  siteFooterName = this.siteData.siteFooterName;
+  constructor(private siteData: SiteDataService) { }
 
   ngOnInit() {
   }
