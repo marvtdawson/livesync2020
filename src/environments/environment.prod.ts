e7e2a52bea2a79fbb2d224645d380a4dc5a03ae3
Marvin Dawson <marvintdawson@gmail.com>
2021-06-26 14:35:32 -0500

    add psuedo prod token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  tokenName: 'Prod-Token',
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' +
+      '.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImdldHZyb20iLCJpYXQiOjE1MTYyMzkwMjJ9' +
+      '.96yaDrMCXtUedcXCboz27PB7kki1K-aG1NTdtdjo4tg'
 };
--- src/services/site-data/site-data.service.ts
+++ src/services/site-data/site-data.service.ts
@@ -5,12 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class SiteDataService {
 
-  siteName = 'Live Sync';
-  siteFooterName = 'Live Sync LLC';
+  siteName = 'Mic Sync';
+  siteFooterName = 'Mic Sync LLC';
   siteTagLine = 'Sync your live performances';
   siteLogo = './assets/img/logo/MicrophoneDropArtboard 1JPG.jpg';
-  siteKeywords = 'Live Sync, Live, Sync, App, Live Sync App, Application' +
-      'Mobile App, Mobile, Open, Mic, Open Mic, Performance, Perform' +
+  siteKeywords = 'Mic Sync, Mic, Sync, App, Mic Sync App, Application, micsync, micsync.com' +
+      'Mobile App, Mobile, Open, Open Mic, Performance, Perform' +
       'Marvin Dawson, Dawson Technical Consulting';
   year: number = new Date().getFullYear();
   constructor() { }
