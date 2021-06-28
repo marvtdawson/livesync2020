1257f400b9703609b4d42adf1786e35052155321
Marvin Dawson <marvintdawson@gmail.com>
2021-06-28 17:37:24 -0500

    added mock data and fan display

--- src/app/admin/fans/fans.page.ts
+++ src/app/admin/fans/fans.page.ts
@@ -8,6 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class FansPage implements OnInit {
 
   pageTitle = 'Admin Fans Page';
+  fans = [
+    {'name': 'Chantel', 'title': 'Property Manager', 'imgThumbnail': '/assets/img/admin/employees/employee_9.jpg', 'isOnline': true},
+    {
+      'name': 'Carol',
+      'title': '1st Shift Office Manager',
+      'imgThumbnail': '/assets/img/admin/employees/employee_2.jpg',
+      'isOnline': true
+    },
+    {'name': 'Jasmine',
+      'title': '1st Shift Front Desk Admin',
+      'imgThumbnail': '/assets/img/admin/employees/employee_5.jpg',
+      'isOnline': true},
+    {'name': 'Rick', 'title': 'Front Desk Admin', 'imgThumbnail': '/assets/img/admin/employees/employee_3.jpg', 'isOnline': true},
+    {'name': 'Na`Kia',
+      'title': '2nd Shift Office Manager',
+      'imgThumbnail': '/assets/img/admin/employees/employee_6.jpg',
+      'isOnline': false},
+    {'name': 'Anna', 'title': '2nd Front Desk Admin', 'imgThumbnail': '/assets/img/admin/employees/employee_4.jpg',  'isOnline': false},
+    {'name': 'Herald', 'title': 'Air Condition / Electric', 'imgThumbnail': '/assets/img/admin/employees/employee_7.jpg', 'isOnline': true},
+    {'name': 'Alex', 'title': 'Maintenance / Paint', 'imgThumbnail': '/assets/img/admin/employees/employee_8.jpg', 'isOnline': true},
+    {'name': 'Pete', 'title': 'Grounds', 'imgThumbnail': '/assets/img/admin/employees/employee_10.jpg', 'isOnline': true},
+    {'name': 'Dave', 'title': 'Security', 'imgThumbnail': '/assets/img/admin/employees/employee_1.jpg', 'isOnline': true},
+    {'name': 'Scott', 'title': '2nd Shift Security', 'imgThumbnail': '/assets/img/admin/employees/employee_3.jpg', 'isOnline': false},
+
+  ];
   constructor() { }
 
   ngOnInit() {
