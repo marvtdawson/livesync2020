49f16e6ddc1fb2dc0bd708ac310e014fbe53650e
Marvin Dawson <marvintdawson@gmail.com>
2021-07-13 14:37:24 -0500

    added cordova video player plugin to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { VideoPlayer } from '@ionic-native/video-player/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -34,6 +35,7 @@ import { FIREBASE_CONFIGED } from './app-firebase.config';
   providers: [
     StatusBar,
     SplashScreen,
+    VideoPlayer,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     AngularFireAuth
   ],
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VideoPlayer } from '@ionic-native/video-player/ngx';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class HomePage implements OnInit {
 
   pageTitle = 'Home Page';
-  constructor() { }
+  constructor(private videoPlayer: VideoPlayer) { }
 
   ngOnInit() {
+    this.playLocalVideo();
+  }
+
+  playLocalVideo() {
+    const vidPlayer = this.videoPlayer.play('https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4')
+        .then(() => {
+          console.log('Video Played!!!');
+        })
+        .catch(err => {
+          console.log('We got this error: ' + err);
+        });
   }
 
 }
